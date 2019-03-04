8ec75bc72adb1d51440b7cd4abdbf6cbe95d5b2e
Vincenzo Maffione <v.maffione@gmail.com>
2019-03-04 18:42:47 +0100

    qemu-proxy.sh: add hugepages

--- proxy/qemu-proxy.sh
+++ proxy/qemu-proxy.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+##################### Setup hugepages #######################
+# This is needed by both Snabb and OVS-DPDK.
+# There are various methods: run-time allocation, sysctl,
+# boot parameters, let's go with the simpler.
+# Append 'hugepages=256' to the kernel
+# boot cmdline and reboot the system.
+# This will allocate 256 2M hugepages, and enable IOMMU (VT-d).
+# Enable VT-d in the BIOS, if present.
+#
+# Check that hugepages are there:
+#
+#     $ grep -i huge /proc/meminfo
+#
+# Check that /dev/hugepages is mounted, otherwise mount it:
+#
+#     # mkdir /dev/hugepages
+#     # mount -t hugetlbfs nodev /dev/hugepages
+
 sudo qemu-system-x86_64 /home/vmaffione/git/vm/netmap.qcow2 \
-        -enable-kvm -smp 2 -m 2G -vga std \
+        -enable-kvm -smp 2 -m 512M -vga std \
         -device e1000,netdev=mgmt,mac=00:AA:BB:CC:0a:99 \
         -netdev user,id=mgmt,hostfwd=tcp::20020-:22 \
+        -numa node,memdev=mem0 \
+        -object memory-backend-file,id=mem0,size=512M,mem-path=/dev/hugepages,share=on \
         -device bpfhv-pci,netdev=data20,mac=00:AA:BB:CC:0a:0a \
         -netdev type=bpfhv-proxy,id=data20,chardev=char20 \
         -chardev socket,id=char20,path=/var/run/vm20-20.socket,server
