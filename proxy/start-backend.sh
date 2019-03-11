9de65254600d432b0cd89cc29330c1154b51b6fd
Vincenzo Maffione <v.maffione@gmail.com>
2019-03-11 12:23:58 +0100

    proxy: add script to start proxy

---
+++ proxy/start-backend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IF=tapx
+IPADDR=10.0.0.10/24
+SOCK=/var/run/vm20-20.socket
+
+sigint() {
+    sudo ip link set $IF down
+    sudo ip link del $IF
+}
+trap 'sigint' INT
+
+sudo ip tuntap add mode tap name $IF
+sudo ip link set $IF up
+sudo ip addr add $IPADDR dev $IF
+sudo proxy/backend -p $SOCK -t $IF -v
